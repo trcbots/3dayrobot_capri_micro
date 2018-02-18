7161ea27e570d87a69e09e367062f291a84ce683
Alex McClung <alex.mcclung@hotmail.com>
2018-02-18 12:06:37 +1000

    add pwm_command.h

---
+++ pwm_command.h
@@ -0,0 +1,15 @@
+class PWMCommand {
+    public:
+        PWMCommand();
+        int message_type;                   // [XBOX = 1, JETSON = 2]  
+        int message_ignition;               // Igniton
+        int message_engine_start;           // Start
+        int message_steering;               // Steering
+        int message_velocity;               // Velocity
+        int message_gear;                   // Gear
+        int message_failsafe;               // Failsafe
+};
+
+PWMCommand::PWMCommand()
+{
+}
